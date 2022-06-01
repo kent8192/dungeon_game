c655a4a7072f0eba8cf3e7f8b3441f3d63484ccc
kent8192 <kento.kiku@outlook.jp>
2022-06-01 12:26:36 +0900

    fix module files

---
+++ src/dungeon/event/buttle/mod.rs
@@ -0,0 +1,4 @@
+pub mod battle;
+pub mod field;
+pub mod field_unit;
+pub mod monster;
--- src/dungeon/mod.rs
+++ src/dungeon/mod.rs
@@ -1,9 +1,6 @@
 pub mod dungeon;
 pub mod event;
 pub mod floor;
-pub mod monster;
 
-pub use dungeon::dungeon;
-pub use event::event;
-pub use floor::floor;
-pub use monster::monster;
+pub use dungeon::Dungeon;
+pub use floor::Floor;
